88e03ac30143354e69a79bcc78a8ba7477f003d6
ggdinesh <dinesh@Dineshs-MacBook-Air.local>
2024-04-06 20:47:46 -0700

    Navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+import Nav from './components/Nav';
+import { Hero, PopularProducts, Services, SpecialOffers, Subscribe, SuperQuality, Footer, CustomerReviews} from './sections'
+
+
+const App = () => (
+  <main className='relative'>
+    <Nav/>
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts/>
+    </section>
+    <section className="padding">
+      <SuperQuality/>
+    </section>
+    <section className="padding-x py-10">
+      <Services/>
+    </section>
+    <section className="padding">
+      <SpecialOffers/>
+    </section>
+    <section className="padding">
+      <CustomerReviews/>
+    </section>
+    <section className="padding">
+      <Subscribe/>
+    </section>
+    <section className="padding">
+      <Footer/>
+    </section>
+  </main>
+);
+
+export default App;
